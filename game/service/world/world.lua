45a29f9906622571537b4f591c46101816967b43
mrq <362651591@qq.com>
2014-11-06 23:51:57 +0800

    Create world.lua

---
+++ game/service/world/world.lua
@@ -0,0 +1,16 @@
+
+local skynet = require "skynet"
+local util = require "util"
+local mongo = require "lmongo"
+local mc = mongo.init(".mongodb",skynet.call,skynet.send)
+
+skynet.start(function()
+	local r = mc.u3d.role:findAll()
+	util.dump_table(r)
+	skynet.register(".world")
+end)
+
+skynet.dispatch("lua", function(session, address, cmd , ...)
+	command[cmd](...)
+end)
+
